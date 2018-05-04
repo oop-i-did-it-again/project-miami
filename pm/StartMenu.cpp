38af59ae11d40071b32a223eca7f650c5b3955d6
Justin Lok <32407493+justinlok@users.noreply.github.com>
2018-05-04 12:52:27 -0700

    random splash screen

--- pm/StartMenu.cpp
+++ pm/StartMenu.cpp
@@ -1,17 +1,21 @@
 #include "StartMenu.h"
 #include <iostream>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h> 
 
 StartMenu::StartMenu(){
 	init();
 }
 
 void StartMenu::init(){
+	 srand (time(NULL));
    background[0]=new TexRect("assets/title1.bmp",1,1,-1,1,2,2);
    background[1]=new TexRect("assets/title2.bmp",1,1,-1,1,2,2);
    background[2]=new TexRect("assets/title3.bmp",1,1,-1,1,2,2);
    background[3]=new TexRect("assets/title4.bmp",1,1,-1,1,2,2);
    background[4]=new TexRect("assets/title5.bmp",1,1,-1,1,2,2);
-   quality = 0;
+   quality = rand() % 4;
 }
 
 void StartMenu::draw(){
